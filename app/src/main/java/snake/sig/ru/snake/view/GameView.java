94d3a0bfc930fd477176f5c308ccd71dc7e2cee7
Flax163 <goncharov.valia@yandex.ru>
2014-09-27 21:57:23 +0400

    update view

--- app/src/main/java/snake/sig/ru/snake/view/GameView.java
+++ app/src/main/java/snake/sig/ru/snake/view/GameView.java
@@ -1,8 +1,26 @@
 package snake.sig.ru.snake.view;
 
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.view.View;
+
 /**
  * Created by Developer on 27.09.2014.
  */
-public class GameView
+public class GameView extends View
 {
+    public GameView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public GameView(Context context) {
+        super(context);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+    }
 }
