fce01796078ab69bb7f6f623d652703d518126d5
Affected70 <Affected700@yandex.ru>
2014-09-27 22:22:11 +0400

    added empty methods for Game Logic and created Snake Timer

--- app/src/main/java/snake/sig/ru/snake/controller/GameLogic.java
+++ app/src/main/java/snake/sig/ru/snake/controller/GameLogic.java
@@ -1,8 +1,63 @@
 package snake.sig.ru.snake.controller;
 
+import java.util.List;
+
+import snake.sig.ru.snake.model.FieldNode;
+import snake.sig.ru.snake.model.Snake;
+import snake.sig.ru.snake.view.GameView;
+
 /**
  * Created by Alexander Ionov on 27.09.14.
  */
 public class GameLogic
 {
+    private GameView snakeView;
+    private Snake snake;
+    private int fieldHeight;
+    private int fieldWidth;
+    private List<FieldNode> food;
+    private List<FieldNode> obstacles;
+    private SnakeTimer timer;
+    private int difficulty;
+
+    public void startGame(int difficulty)
+    {
+
+    }
+
+    public void pause()
+    {
+
+    }
+
+    public void resume()
+    {
+
+    }
+
+    public void exit()
+    {
+
+    }
+
+    private int move()
+    {
+        return Integer.parseInt(null);
+    }
+
+    private boolean checkState()
+    {
+        return false;
+    }
+
+    public void changeDirection(int x, int y)
+    {
+
+    }
+
+
+
+
+
+
 }
---
+++ app/src/main/java/snake/sig/ru/snake/controller/SnakeTimer.java
@@ -0,0 +1,9 @@
+package snake.sig.ru.snake.controller;
+
+/**
+ * Created by Aleksandr Shindin on 27.09.2014.
+ */
+public class SnakeTimer
+{
+
+}
