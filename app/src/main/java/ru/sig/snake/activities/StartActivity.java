db5bef7c59e75df4628d98e7dd735563f2597666
Affected70 <Affected700@yandex.ru>
2014-09-28 23:56:53 +0400

    start menu

---
+++ app/src/main/java/ru/sig/snake/activities/StartActivity.java
@@ -0,0 +1,27 @@
+package ru.sig.snake.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import ru.sig.snake.R;
+
+/**
+ * Created by Aleksandr Shindin on 28.09.2014.
+ */
+public class StartActivity extends Activity
+{
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void startGame(View view)
+    {
+        Intent intent = new Intent(StartActivity.this,GameActivity.class);
+        startActivity(intent);
+    }
+}
